deb55f74da3f595d03b8a6c13f8a1603600d3d08
Erika Rodriguez <rodriguez.eri92@gmail.com>
2022-09-22 18:23:31 -0300

    Entity

---
+++ src/main/java/com/solvd/entity/Account.java
@@ -0,0 +1,80 @@
+package entity;
+
+import java.sql.Date;
+
+public class Account {
+    private Integer id_account;
+    private Integer customerId;
+    private Integer branchId;
+    private Integer accountStatusId;
+    private Date openingDate;
+    private int balance;
+
+    public Account( Integer customerId, Integer branchId, Integer accountStatusId, Date openingDate, int balance) {
+        this.customerId = customerId;
+        this.branchId = branchId;
+        this.accountStatusId = accountStatusId;
+        this.openingDate = openingDate;
+        this.balance = balance;
+    }
+
+    public Integer getId_account() {
+        return id_account;
+    }
+
+    public void setId_account(Integer id_account) {
+        this.id_account = id_account;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(Integer branchId) {
+        this.branchId = branchId;
+    }
+
+    public Integer getAccountStatusId() {
+        return accountStatusId;
+    }
+
+    public void setAccountStatusId(Integer accountStatusId) {
+        this.accountStatusId = accountStatusId;
+    }
+
+    public Date getOpeningDate() {
+        return openingDate;
+    }
+
+    public void setOpeningDate(Date openingDate) {
+        this.openingDate = openingDate;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id_account=" + id_account +
+                ", customerId=" + customerId +
+                ", branchId=" + branchId +
+                ", accountStatusId=" + accountStatusId +
+                ", openingDate=" + openingDate +
+                ", balance=" + balance +
+                '}';
+    }
+}
---
+++ src/main/java/com/solvd/entity/Department.java
@@ -0,0 +1,34 @@
+package entity;
+
+public class Department {
+    private Integer id_department;
+    private String departmentName;
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public Integer getId_department() {
+        return id_department;
+    }
+
+    public void setId_department(Integer id_department) {
+        this.id_department = id_department;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id_department=" + id_department +
+                ", departmentName='" + departmentName + '\'' +
+                '}';
+    }
+}
