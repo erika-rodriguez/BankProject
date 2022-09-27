962822fe0e61450a1de1c0ac9c1706b1659be0a7
Erika Rodriguez <rodriguez.eri92@gmail.com>
2022-09-27 11:29:49 -0300

    JAXB, marshal/unmarshal list of Objtect

--- src/main/java/com/solvd/JAXB/CustomerListJAXB.java
+++ src/main/java/com/solvd/JAXB/CustomerListJAXB.java
@@ -1,11 +1,16 @@
 package com.solvd.JAXB;
 
+import com.solvd.Main;
 import com.solvd.daoImplementation.DB_Connection;
 import com.solvd.entity.Customer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -16,6 +21,7 @@ import java.util.List;
 @XmlRootElement(name = "customers")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CustomerListJAXB  {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     static DB_Connection manager=new DB_Connection();
     @XmlElement(name = "customer")
     private List<Customer> customerList = null;
@@ -39,6 +45,20 @@ public class CustomerListJAXB  {
 
         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         jaxbMarshaller.marshal(customers, new File("D:\\Documentos\\Solvd\\Task\\Bank\\src\\main\\resources\\XML/customersList.xml"));
+    }
+
+    public static void unmarshalCustomers() throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(CustomerListJAXB.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+        CustomerListJAXB cust = (CustomerListJAXB) jaxbUnmarshaller.unmarshal( new File("D:\\Documentos\\Solvd\\Task\\Bank\\src\\main\\resources\\XML/customersList.xml"));
+        for (Customer c:cust.getCustomerList()) {
+            logger.info("ID_Customer: "+c.getId_customer());
+            logger.info("Name: "+c.getFullName());
+            logger.info("City: "+c.getCity());
+            logger.info("DOB: "+c.getDateOfBirth());
+            logger.info("Mail: "+c.getMail());
+        }
     }
 
 }
--- src/main/java/com/solvd/JAXB/EmployeeListJAXB.java
+++ src/main/java/com/solvd/JAXB/EmployeeListJAXB.java
@@ -1,11 +1,16 @@
 package com.solvd.JAXB;
 
+import com.solvd.Main;
 import com.solvd.daoImplementation.DB_Connection;
 import com.solvd.entity.Employee;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -16,6 +21,7 @@ import java.util.List;
 @XmlRootElement(name = "employees")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class EmployeeListJAXB {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     static DB_Connection manager=new DB_Connection();
     @XmlElement(name = "employee")
     private List<Employee> employeeList = null;
@@ -41,6 +47,20 @@ public class EmployeeListJAXB {
 
         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         jaxbMarshaller.marshal(employees, new File("D:\\Documentos\\Solvd\\Task\\Bank\\src\\main\\resources\\XML/employeesList.xml"));
+    }
+
+    public static void unmarshalEmployees() throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(EmployeeListJAXB.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+        EmployeeListJAXB emps = (EmployeeListJAXB) jaxbUnmarshaller.unmarshal( new File("D:\\Documentos\\Solvd\\Task\\Bank\\src\\main\\resources\\XML/employeesList.xml"));
+        for (Employee e:emps.getEmployeeList()) {
+            logger.info("ID_Employee: "+e.getId_employee());
+            logger.info("Name: "+e.getFullName());
+            logger.info("Department_ID: "+e.getDepartmentId());
+            logger.info("DOB: "+e.getBirth_date());
+            logger.info("HireDate: "+e.getHire_date());
+        }
     }
 
 
