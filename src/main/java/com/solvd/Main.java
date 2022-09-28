ea81468e1b48f4ae35fa073aff118cfe6e8a8eaf
Erika Rodriguez <rodriguez.eri92@gmail.com>
2022-09-28 12:05:00 -0300

    Json practice

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -7,23 +7,48 @@ import org.apache.logging.log4j.Logger;
 
 import javax.xml.bind.JAXBException;
 
+import java.io.IOException;
+
 import static com.solvd.JAXB.CustomerListJAXB.marshallCustomers;
 import static com.solvd.JAXB.CustomerListJAXB.unmarshalCustomers;
 import static com.solvd.JAXB.EmployeeListJAXB.marshallEmployees;
 import static com.solvd.JAXB.EmployeeListJAXB.unmarshalEmployees;
+import static com.solvd.Jackson.AccountStatusJackson.marshalAccountStatus;
+import static com.solvd.Jackson.AccountStatusJackson.unmarshalAccountStatus;
+import static com.solvd.Jackson.BranchJackson.marshalBranch;
+import static com.solvd.Jackson.BranchJackson.unmarshalBranch;
+import static com.solvd.Jackson.DepartmentJackson.marshalDepartments;
+import static com.solvd.Jackson.DepartmentJackson.unmarshalDepartments;
+import static com.solvd.Jackson.TransactionTypeJackson.marshalTransactionType;
+import static com.solvd.Jackson.TransactionTypeJackson.unmarshalTransactionType;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
-    public static void main(String[] args) throws JAXBException {
+    public static void main(String[] args) throws JAXBException, IOException {
         DB_Connection manager=new DB_Connection();
 
-//        //Marshal list of Customers
+        //Marshal/Unmarshal Departments JACKSON
+        //marshalDepartments();
+        //unmarshalDepartments();
+
+        //Marshal/Unmarshal AccountStatus JACKSON
+        //marshalAccountStatus();
+        //unmarshalAccountStatus();
+
+        //Marshal/Unmarshal TransactionType JACKSON
+        //marshalTransactionType();
+        //unmarshalTransactionType();
+
+        //Marshal/Unmarshal Branch JACKSON
+        //marshalBranch();
+        //unmarshalBranch();
+
+
+//        //Marshal/Unmarshal list of Customers JAXB
 //        marshallCustomers();
-//        //Unmarshal list of Employees XML
 //        unmarshalCustomers();
-//        //Marshal list of Employees
+//        //Marshal/Unmarshal list of Employees JAXB
 //        marshallEmployees();
-//        //Unmarshal list of Employees XML
 //        unmarshalEmployees();
 
 
